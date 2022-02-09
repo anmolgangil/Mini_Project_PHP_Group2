2e1477822d8bc0340351ba7e4cae615a3c51d9ba
asawarika <asawarika@cybage.com>
2022-02-09 12:05:38 +0530

    login and registration with json server completed

---
+++ js/login.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+    $("#login").click(function () {
+        //collect userName and password entered by users
+        var userName = $("#email").val();
+        console.log(userName);
+        var password = $("#password").val();
+        console.log(password);
+
+        auth(userName, password);
+    });
+});
+
+//authenticate function to make ajax call
+function auth(userName, password) {
+    $.ajax({
+        
+        //SEND TO MY SERVER URL
+        url: "http://localhost:3000/user",
+        method: "GET",
+
+
+        data: '{"userName": "' + userName + '", "password" : "' + password + '"}',
+        success: function (data) {
+         $.each(data, function(key, value){ 
+
+            console.log(value.userName);
+            if (value.email == userName && value.password == password) {
+                alert("welcome "+userName);
+                $(location).attr('href', "../html/template.html")
+                return true;
+            }
+        });
+        },
+        error: function (data) {
+            alert("Something went wrong");
+        }
+    })
+}
+
+// $(document).ready(() => {
+
+//     $("#submit").click(() => {
+//         var name = $("#username").val();
+//         console.log(name);
+//         var password = $("#password").val();
+//         console.log(password);
+
+//         if (name == '' && password == '') {
+//             alert("username or password is not entered.");
+//         }
+//         else {
+//             $.ajax({
+//                 // type:"GET",
+//                 URL: "https://mocki.io/v1/bb987962-1cb8-4cf0-9774-daa4fa6a5949",
+//                 method: "GET",
+//                 sucess: function (result) {
+//                     if (result.username == name && result.password == password) {
+//                         alert("sucess");
+//                         $(location).attr('href', "../html/home.html")
+//                     }
+//                 },
+//                 error: (result) => {
+//                     alert("Something went wrong");
+//                 }
+//             });
+
+//         }
+//     });
+// })
---
+++ js/register.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+    $("#register").click(function () {
+        var name = $("#uname1").val();
+        var email = $("#email1").val();
+        var password = $("#password1").val();
+
+        var uppercase = /[A-Z]/g;
+        var numbers = /[0-9]/g;
+       
+      if (name.value.length ==0){
+         alert("field required");
+      } else if (email.val.length ==0){
+      alert("field required");
+   } else if (password.val.length ==0){
+       alert("field required");
+   
+   }  else if (password.val.length > 8){
+       alert("max of 8");
+   } else if (!password.val.match(numbers)){
+        alert("please add 1 number");
+
+     }else if (!password.val.match(uppercase)){
+      alert("please add 1 uppercase letter");
+    }else {
+
+      alert("success.....")
+
+   }
+
+        var formData = {
+            
+            "name": name,
+            
+            "email": email,
+            
+            "password": password
+            
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:3000/user",
+            data: formData,
+            dataType:"json",
+            success: function (response) {
+                alert("Registered Successfully...!!");
+            }
+        });
+    });
+
+});
+
+    
